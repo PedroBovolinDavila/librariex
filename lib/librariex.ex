10099ff09d0c5dc2e4d683b4ce0bc75904aca156
Pedro Davila <pedrodavila.bovolin@gmail.com>
2022-06-25 20:53:12 -0300

    :sparkles: Added user crud delegates

--- lib/librariex.ex
+++ lib/librariex.ex
@@ -1,9 +1,12 @@
 defmodule Librariex do
-  @moduledoc """
-  Librariex keeps the contexts that define your domain
-  and business logic.
+  alias Librariex.Users.Create, as: CreateUser
+  alias Librariex.Users.Delete, as: DeleteUser
+  alias Librariex.Users.Get, as: GetUser
+  alias Librariex.Users.Update, as: UpdateUser
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  defdelegate create_user(params), to: CreateUser, as: :call
+  defdelegate get_users(), to: GetUser, as: :call
+  defdelegate get_user_by_id(id), to: GetUser, as: :by_id
+  defdelegate delete_user(id), to: DeleteUser, as: :call
+  defdelegate update_user(id), to: UpdateUser, as: :call
 end
